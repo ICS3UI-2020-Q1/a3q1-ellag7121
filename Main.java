e9d368e198b27ad0e610c0f8c0c2b3b00ada14f7
ellag7121 <71140771+ellag7121@users.noreply.github.com>
2020-10-01 10:54:56 -0400

    Finished A3Q1

--- Main.java
+++ Main.java
@@ -1,6 +1,7 @@
+import java.util.Scanner;
 /**
- *
- * @author 
+ * asks user for 3 integers, tells the user which one is greatest
+ * @author Graham Ellacott
  */
 public class Main {
 
@@ -9,7 +10,29 @@ public class Main {
    * @param args the command line arguments
    */
   public static void main(String[] args) {
-    // TODO code application logic here
+    // create a scanner to get input
+    Scanner input = new Scanner(System.in);
+    
+    //initialize variables
+    int number1; //the first number provided by the user
+    int number2; //the second number provided by the user
+    int number3; //the third number provided by the user
+
+    //ask user for integers
+    System.out.println("Please enter three integers on separate lines");
+    //store those values in the number variables created earlier
+    number1 = input.nextInt(); //store first number in the variable
+    number2 = input.nextInt(); //store second number in the variable
+    number3 = input.nextInt(); //store third number in the variable
+
+    //check to see which number is the greatest
+    if (number1 > number2 && number1 > number3){ //check if first number is greater than 2nd and 3rd
+      System.out.println("Maximum: " + number1); //tell user first number is greatest
+    }else if (number2 > number1 && number2 > number3){ //check if second number is greater than 1nd and 3rd
+      System.out.println("Maximum: " + number2); //tell user second number is greatest
+    }else if (number3 > number1 && number3 > number2){ //check if third number is greater than 1st and 2nd
+      System.out.println("Maximum: " + number3); //tell user third number is greatest
+    }
     
   }
 }
